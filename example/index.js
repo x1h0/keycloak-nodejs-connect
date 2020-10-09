763886f03f08ded8b75e028b9382977bb7d82bb4
Bruno Oliveira da Silva <bruno@abstractj.org>
2020-10-09 19:24:40 -0300

    Cookie store

--- example/index.js
+++ example/index.js
@@ -1,92 +1,31 @@
-/*
- * Copyright 2016 Red Hat Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
-var Keycloak = require('keycloak-connect');
-var hogan = require('hogan-express');
 var express = require('express');
+var Keycloak = require('keycloak-connect');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var serveIndex = require('serve-index');
 
 var app = express();
+app.use(morgan('combined'));
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
-});
-
-// Register '.mustache' extension with The Mustache Express
-app.set('view engine', 'html');
-app.set('views', require('path').join(__dirname, '/view'));
-app.engine('html', hogan);
-
-// A normal un-protected public URL.
-
-app.get('/', function (req, res) {
-  res.render('index');
-});
-
-// Create a session-store to be used by both the express-session
-// middleware and the keycloak middleware.
-
-var memoryStore = new session.MemoryStore();
-
-app.use(session({
-  secret: 'mySecret',
-  resave: false,
+const secret = 'mae7eiD0EeVao9th';
+app.use(cookieParser());
+app.use(session({ // keycloak-connect requires a session id
+  secret: secret,
+  resave: true,
   saveUninitialized: true,
-  store: memoryStore
+  name: 'session'
 }));
-
-// Provide the session store to the Keycloak so that sessions
-// can be invalidated from the Keycloak console callback.
-//
-// Additional configuration is read from keycloak.json file
-// installed from the Keycloak web console.
 
 var keycloak = new Keycloak({
-  store: memoryStore
+  cookies: true // only use for debugging - tokens will be accessible in the browser unencrypted
 });
-
-// Install the Keycloak middleware.
-//
-// Specifies that the user-accessible application URL to
-// logout should be mounted at /logout
-//
-// Specifies that Keycloak console callbacks should target the
-// root URL.  Various permutations, such as /k_logout will ultimately
-// be appended to the admin URL.
-
 app.use(keycloak.middleware({
   logout: '/logout',
-  admin: '/',
-  protected: '/protected/resource'
+  admin: '/'
 }));
 
-app.get('/login', keycloak.protect(), function (req, res) {
-  res.render('index', {
-    result: JSON.stringify(JSON.parse(req.session['keycloak-token']), null, 4),
-    event: '1. Authentication\n2. Login'
-  });
-});
+const dir = '.';
+app.use('/', keycloak.protect(), express.static(dir), serveIndex(dir));
 
-app.get('/protected/resource', keycloak.enforcer(['resource:view', 'resource:write'], {
-  resource_server_id: 'nodejs-apiserver'
-}), function (req, res) {
-  res.render('index', {
-    result: JSON.stringify(JSON.parse(req.session['keycloak-token']), null, 4),
-    event: '1. Access granted to Default Resource\n'
-  });
-});
+app.listen(3000);
